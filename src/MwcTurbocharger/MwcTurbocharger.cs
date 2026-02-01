19cd4b72a9f3ff9acc461af4c9af4ff0fe7c977b
Marvin Beym <Marvin.Beym@gmx.de>
2026-02-01 11:57:23 +0100

    Add ShopItem product images

--- src/MwcTurbocharger/MwcTurbocharger.cs
+++ src/MwcTurbocharger/MwcTurbocharger.cs
@@ -216,12 +216,12 @@ namespace MwcTurbocharger
 			shop.Add(
 				shopBaseInfo, shopLocation, new[]
 				{
-					new ShopItem("Turbocharger Kit", 8100, shopSpawnLocation, turboBigKit),
-					new ShopItem("Turbocharger Blowoff Valve", 1350, shopSpawnLocation, turboBigBlowoffValve),
-					new ShopItem("Racing Carb Manifold Kit", 4000, shopSpawnLocation, racingCarbManifoldKit),
-					new ShopItem("Intercooler", 3000, shopSpawnLocation, intercooler),
-					new ShopItem("Boost Gauge", 180, shopSpawnLocation, boostGauge),
-					new ShopItem("Turbocharger Exhaust Header", 2100, shopSpawnLocation, exhaustHeader),
+					new ShopItem("Turbocharger Kit", 8100, shopSpawnLocation, turboBigKit, "turboBig-kit.png"),
+					new ShopItem("Turbocharger Blowoff Valve", 1350, shopSpawnLocation, turboBigBlowoffValve, "turboBig-blowoff-valve.png"),
+					new ShopItem("Racing Carb Manifold Kit", 4000, shopSpawnLocation, racingCarbManifoldKit, "racingCarb-manifold-kit.png"),
+					new ShopItem("Intercooler", 3000, shopSpawnLocation, intercooler, "intercooler.png"),
+					new ShopItem("Boost Gauge", 180, shopSpawnLocation, boostGauge, "boost-gauge.png"),
+					new ShopItem("Turbocharger Exhaust Header", 2100, shopSpawnLocation, exhaustHeader, "exhaust-header"),
 				}
 			);
 		}
